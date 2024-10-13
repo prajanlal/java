12277d4329d0a55e23544e76210f620a68fb0236
prajanlal <162490198+prajanlal@users.noreply.github.com>
2024-10-13 12:37:51 +0530

    newthread.java file

---
+++ newthread.java
@@ -0,0 +1,43 @@
+
+public class NewThread implements Runnable {
+    Thread t;
+
+    Newthread() {
+        t = new Thread(this, "Demothread");
+        System.out.println("child thread" + t);
+        t.start();
+    }
+
+    public void run() {
+        {
+            try {
+                for (int i = 5; i > 0; i--) {
+                    System.out.println("child thread: "+ i);
+                    Thread.sleep(500);c
+                }
+            } catch (InterruptedException e) {
+                System.out.println("child thread Interrupted");
+                System.out.println("child thread Exit");
+            }
+        }
+        class demothread {
+            public static void main(String args[]) {
+                new NewThread();
+                {
+                    try {
+                        for (int i = 5; i > 0; i--) {
+                            System.out.println("Main thread: "+ i);
+                            Thread.sleep(1000);
+                        }
+                    } catch (InterruptedException e) {
+                        System.out.println("Main thread Interrupted");
+                    }
+                    System.out.println("Main thread Exit");
+                }
+            }
+        }
+    }
+}
+
+
+
