5ff608a52d1db0e3b4765efab80385ac5bdacb1b
PRAJAN LAL <162490198+prajanlal@users.noreply.github.com>
2024-10-14 22:14:54 +0530

    Create copyfile.java

---
+++ copyfile.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class copyfile {
+    public static void main(String args[]) {
+        throw IOException {
+            FileReader in = null;
+            FileWriter out = null;
+            try {
+                in = new FileReader("java.txt");
+                out = new FileWriter("arun.txt");
+                int c;
+                while ((c = in.read()) != -1) {
+                    out.write(c);
+                }
+            } finally {
+                if (in != null) {
+                    in.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+            }
+        }
+    }
+}
