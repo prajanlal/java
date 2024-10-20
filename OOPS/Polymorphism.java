6e364515950b7fe396d19cec85c9b9c23bf653ba
PRAJAN LAL <162490198+prajanlal@users.noreply.github.com>
2024-10-20 13:25:48 +0530

    Create Polymorphism.java

---
+++ OOPS/Polymorphism.java
@@ -0,0 +1,17 @@
+public class car{
+    public void sportcar() {
+        System.out.println("bmw is a sports car");
+    }
+
+    class topengine {
+        public void sportcar() {
+            System.out.println("bmw is a super sport car");
+        }
+
+        class topspeed {
+            public void speed() {
+                System.out.println("BUGGATI has world record for top speed");
+            }
+        }
+    }
+}
