3b283394afd1d9bd83f73189d7322bf75c09e210
PRAJAN LAL <162490198+prajanlal@users.noreply.github.com>
2024-10-24 09:47:48 +0530

    Create Threedimensionalarray.java

---
+++ ARRAYS/Threedimensionalarray.java
@@ -0,0 +1,25 @@
+public class ThreeDArray {
+public static void main(String[] args) 
+{
+    int[ ][ ][ ] x;
+         x = new int[3][3][3];
+
+    for(int i = 0; i < 3; i++)
+    {
+       for(int j = 0; j < 3; j++)	 
+           for(int k = 0; k < 3; k++)
+	       x[i][j][k] = i + 1;
+    }
+    for(int i = 0; i < 3; i++)
+    {
+        System.out.println("Table-" +(i + 1));
+        for(int j = 0; j < 3; j++)
+        {
+	   for(int k = 0; k < 3; k++)
+	   System.out.print(x[i][j][k] +" ");	 
+	   System.out.println();
+        }
+        System.out.println();  
+    }
+  }
+}
