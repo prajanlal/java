d41bd78f859a344d89503094daab0d019e8cfb44
PRAJAN LAL <162490198+prajanlal@users.noreply.github.com>
2024-10-24 07:01:13 +0530

    Create Array.java

---
+++ ARRAY/Array.java
@@ -0,0 +1,13 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[5];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i * 10; 
+        }
+        System.out.println("Array values:");
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
