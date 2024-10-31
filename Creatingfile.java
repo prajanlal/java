5741aeaadeb55d9abe9f20889a15b8011c4c5ab7
PRAJAN LAL <162490198+prajanlal@users.noreply.github.com>
2024-10-31 11:34:13 +0530

    Create Creatingfile.java

---
+++ Creatingfile.java
@@ -0,0 +1,9 @@
+import java.io.File;
+class Creatingfile{
+  public static void main(String[] args){
+    File obj=new File("myfile.txt");
+    System.out.println("File Created!");
+  }
+}
+
+      
