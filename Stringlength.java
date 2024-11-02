e9968e75a2d091df2309ef8736bbe0ee49e5d2ce
PRAJAN LAL <162490198+prajanlal@users.noreply.github.com>
2024-11-02 20:17:01 +0530

    Create  Stringlength.java

---
+++ Stringlength.java
@@ -0,0 +1,19 @@
+public class LengthExample4  
+{  
+// main method  
+public static void main(String argvs[])  
+{  
+String str = " Welcome To JavaTpoint ";  
+int sizeWithWhiteSpaces = str.length();  
+  
+System.out.println("In the string: " + "'" + str + "'");  
+  
+str = str.replace(" ", "");  
+int sizeWithoutWhiteSpaces = str.length();  
+  
+// calculating the white spaces  
+int noOfWhieSpaces = sizeWithWhiteSpaces - sizeWithoutWhiteSpaces;  
+  
+System.out.print("Total number of whitespaces present are: " + noOfWhieSpaces);  
+}  
+} 
