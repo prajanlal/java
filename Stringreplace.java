29513315f6deb87e9108229199513625360f15d9
PRAJAN LAL <162490198+prajanlal@users.noreply.github.com>
2024-11-03 15:01:39 +0530

    Create Stringreplace.java

---
+++ Stringreplace.java
@@ -0,0 +1,7 @@
+public class StringReplaceExample {
+    public static void main(String[] args) {
+        String original = "Hello, World!";
+        String modified = original.replace("World", "Java");
+        System.out.println(modified);  // Outputs: Hello, Java!
+    }
+}
