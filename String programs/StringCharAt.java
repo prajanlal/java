e53962d40d6e39de5f181ee88762df3c7c71859d
PRAJAN LAL <162490198+prajanlal@users.noreply.github.com>
2024-11-05 10:33:03 +0530

    Create StringCharAt.java

---
+++ String programs/StringCharAt.java
@@ -0,0 +1,15 @@
+public class CharAtExample {
+
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+
+        // Get the character at index 0 (the first character)
+        char firstChar = str.charAt(0); 
+
+        // Get the character at index 7
+        char seventhChar = str.charAt(7);
+
+        System.out.println("First character: " + firstChar);
+        System.out.println("Seventh character: " + seventhChar);
+    }
+}
