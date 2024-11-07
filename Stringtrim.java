5623b15a7f61473527e44b0ee704612726e78922
PRAJAN LAL <162490198+prajanlal@users.noreply.github.com>
2024-11-07 19:01:43 +0530

    Create StringTrim.java

---
+++ Stringtrim.java
@@ -0,0 +1,9 @@
+public class StringTrimExample {
+    public static void main(String[] args) {
+        String str = "   Hello, World!   ";
+        System.out.println("Original String: '" + str + "'");
+
+        String trimmedStr = str.trim();
+        System.out.println("Trimmed String: '" + trimmedStr + "'");
+    }
+}
