7a5c711690a924a3af475ad9a09989d372c2dcaf
PRAJAN LAL <162490198+prajanlal@users.noreply.github.com>
2024-11-08 17:16:34 +0530

    Create StringEquals.java

---
+++ String programs/StringEquals.java
@@ -0,0 +1,15 @@
+public class StringEqualsExample {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Hello";
+        String str3 = "hello";
+
+        // Using equals() to compare strings
+        boolean result1 = str1.equals(str2);
+        boolean result2 = str1.equals(str3);
+
+        // Print the results
+        System.out.println("str1 equals str2: " + result1); // true
+        System.out.println("str1 equals str3: " + result2); // false
+    }
+}
