3a0971a46b33b8930e54ae90121a4d1fa9e3fcc8
PRAJAN LAL <162490198+prajanlal@users.noreply.github.com>
2024-11-10 17:30:34 +0530

    Create StringSubstring.java

---
+++ String programs/StringSubstring.java
@@ -0,0 +1,13 @@
+public class Substring{
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        
+        // Extracting substring from index 7 to the end
+        String substr1 = str.substring(7);
+        System.out.println("Substring from index 7: " + substr1);
+
+        // Extracting substring from index 0 to 4 (endIndex is exclusive)
+        String substr2 = str.substring(0, 5);
+        System.out.println("Substring from index 0 to 4: " + substr2);
+    }
+}
