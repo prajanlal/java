3daf461f52430cf0f7f94b909c99428356cd0bff
PRAJAN LAL <162490198+prajanlal@users.noreply.github.com>
2024-11-14 21:06:07 +0530

    Create String indexOf.java

---
+++ String indexOf.java
@@ -0,0 +1,19 @@
+public class IndexOf {
+    public static void main(String[] args) {
+        // Sample string
+        String str = "Hello, welcome to the Java world!";
+        
+        // Substring to find
+        String subStr = "Java";
+        
+        // Using indexOf() to find the first occurrence of "Java"
+        int index = str.indexOf(subStr);
+        
+        // Check if the substring was found
+        if (index != -1) {
+            System.out.println("The substring '" + subStr + "' is found at index: " + index);
+        } else {
+            System.out.println("The substring '" + subStr + "' is not found.");
+        }
+    }
+}
