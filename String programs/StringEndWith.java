b10b233d910b615786c285b5bb67035937ed2b88
PRAJAN LAL <162490198+prajanlal@users.noreply.github.com>
2024-11-15 19:39:32 +0530

    Create StringEndWith.java

---
+++ String programs/StringEndWith.java
@@ -0,0 +1,19 @@
+public class EndsWith {
+    public static void main(String[] args) {
+        String str = "Hello, world!";
+        
+        // Check if the string ends with "world!"
+        if (str.endsWith("world!")) {
+            System.out.println("The string ends with 'world!'");
+        } else {
+            System.out.println("The string does not end with 'world!'");
+        }
+        
+        // Check if the string ends with "Hello"
+        if (str.endsWith("Hello")) {
+            System.out.println("The string ends with 'Hello'");
+        } else {
+            System.out.println("The string does not end with 'Hello'");
+        }
+    }
+    }
