8885d677e47b5453d437ddf0481d33a324fe5b87
prajanlal <prajanswags@gmail.com>
2025-02-22 22:21:05 +0530

    Changed the issue

--- GUI/Gui.java
+++ GUI/Gui.java
@@ -1,12 +1,8 @@
 import javax.swing.*;
-<<<<<<< HEAD
 //import java.awt.Container;
 //import java.awt.event.*;
-public class Gui   { 
-=======
 import java.awt.*;
 import java.awt.event.*;
-
 public class Gui implements ActionListener {  
   
     private static JLabel label;
@@ -15,7 +11,6 @@ public class Gui implements ActionListener {
     private static JPasswordField passwordText; 
     private static JLabel Success;
    
->>>>>>> 5763c9e370af961851231e07bb0bcd21db325ca3
     //https://beginnersbook.com/2015/07/java-swing-tutorial
     public static void main(String args[]){ 
         JFrame frame = new JFrame("Calculator"); 
@@ -23,7 +18,7 @@ public class Gui implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
         frame.add(panel);
-<<<<<<< HEAD
+
       
         frame.setVisible(true);
 
@@ -61,52 +56,5 @@ public class Gui implements ActionListener {
     }
           }
            
-          
-         
-
+        
    
-=======
-          
-         panel.setLayout(null);
-  
-        label = new JLabel("User");
-        label.setBounds(10,20,80,25);
-        panel.add(label);
-     
-         userText =new JTextField(50);
-         userText.setBounds(100,20,165,25);
-         panel.add(userText);
-        
-        passwordLabel= new JLabel("Password");
-        passwordLabel.setBounds(10,50,80,25);
-        panel.add(passwordLabel);
-        
-        passwordText = new JPasswordField(50);
-        passwordText.setBounds(100,50,165,25);
-        panel.add(passwordText);
-         
-       JButton button = new JButton("LOGIN");
-       button.addActionListener(new GUI());
-       button.setBounds(10,80,80,25);
-       panel.add(button); 
-  
-        JLabel Success = new JLabel("");
-        Success.setBounds(10,110,300,25);
-        panel.add(Success);
-         
-        frame.setVisible(true);
-    }
-  
-    @Override
-    public void actionPerformed(ActionEvent e) { 
-          String user = userText.getText();      
-         
-      
-          System.out.println(user + " "); 
-         
-           if(user.equals("Prajan Lal")){
-             Success.setText("Login Successful!");
-           }
-      }
->>>>>>> 5763c9e370af961851231e07bb0bcd21db325ca3
-
