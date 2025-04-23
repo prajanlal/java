40c95120c6d81354a779a890dd4a9931036160ff
prajanlal <prajanswags@gmail.com>
2025-04-23 21:46:34 +0530

    fix: Solved some problem

--- Interface/Car.java
+++ Interface/Car.java
@@ -2,45 +2,40 @@ public class Car implements Vehicle {
       String Brand;
       String Model;
       String limit;
-      String MadeIn;
+      
         @Override
         public void setBrand(String Brand){
-         this.brand = brand;
+         this.Brand = Brand;
         }
         public String getBrand(){
-         return brand;
+         return Brand;
         }
         public void setModel(String Model){
-         this.model = model;
+         this.Model = Model;
         }
         public String getModel(){
-         return model;
+         return Model;
         }
         public void setlimit(String limit){
          this.limit = limit;
         }
         public String getlimit(){
          return limit;
-        }
-        pubilc void MadeIn(String MadeIn){
-         this.MadeIn = MadeIn;
-        }
-        public String getMadeIn(){
-         return MadeIn;
+        
         }
        public void start(){
           System.out.println("The car is starting");
        }
-      public Car(String Carbrand,String CarModel,String SpeedLimit,String Country) {
+      public Car(String Carbrand,String CarModel,String SpeedLimit) {
                this.Brand = Carbrand;
                this.Model = CarModel;
                this.limit = SpeedLimit;
-               this.MadeIn = Country;
+               
        }
     public void display(){
       System.out.println("The car brand is: "+Brand);
       System.out.println("The car model name is: "+Model);
       System.out.println("The car speed limit is"+limit);
-      System.out.println("The Car origin was in: "+MadeIn);
+   
 }
 }
