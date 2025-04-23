0c6094d02a6dc58eafcd48ddd101810073620c5f
prajanlal <prajanswags@gmail.com>
2025-04-23 20:54:05 +0530

    fix:Added speed limit to the bike class

--- Interface/Bike.java
+++ Interface/Bike.java
@@ -1,7 +1,14 @@
-public abstract class Bike implements Vehicle {
+public  class Bike implements Vehicle {
     public void Start(){
         System.out.print("Kawasaki has stated");
      
                 }
-    }   
+                public static void Speed(){
+                    System.out.print("The speed has limited to 100kmph");
+                }
 
+    @Override
+    public void start() {
+        throw new UnsupportedOperationException("Not supported yet.");
+      }
+    }   
