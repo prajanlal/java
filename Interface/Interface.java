32d7c6fad74b3ea07b07d2a667ae046efc00f29e
prajanlal <prajanswags@gmail.com>
2025-04-25 22:49:05 +0530

    Feat: Added a new Interface class as the main class

---
+++ Interface/Interface.java
@@ -0,0 +1,8 @@
+public class Interface {
+    public static void main(String[] args){
+            Vehicle Bike = new Bike();
+            
+            Bike.Engine();
+            Bike.HP();
+    }
+} 
