7339c8d087f8e23dac5add63848007ccd4b03609
prajanlal <prajanswags@gmail.com>
2025-04-25 22:26:06 +0530

    Fix:Changed the Main.java to Constructor.java

---
+++ ARRAYS/Constructor.java
@@ -0,0 +1,11 @@
+public class Constructor {
+
+    public static void main(String args[]){
+
+         Car cars = new Car("BMW","M4",800);
+          cars.display();
+
+         Bike bike = new Bike("KAWASAKI","ZX10R",800,"JAPAN");
+         bike.display();
+    }
+}
