88ca8acc4c29a141f985bda9efe61cff21239dca
prajanlal <prajanswags@gmail.com>
2025-04-26 17:39:21 +0530

    Feat: Added the maximum speed

--- Interface/Car.java
+++ Interface/Car.java
@@ -1,41 +1,14 @@
 public class Car implements Vehicle {
-      String Brand;
-      String Model;
-      String limit;
-      
-        @Override
-        public void setBrand(String Brand){
-         this.Brand = Brand;
-        }
-        public String getBrand(){
-         return Brand;
-        }
-        public void setModel(String Model){
-         this.Model = Model;
-        }
-        public String getModel(){
-         return Model;
-        }
-        public void setlimit(String limit){
-         this.limit = limit;
-        }
-        public String getlimit(){
-         return limit;
-        
-        }
-       public void start(){
-          System.out.println("The car is starting");
-       }
-      public Car(String Carbrand,String CarModel,String SpeedLimit) {
-               this.Brand = Carbrand;
-               this.Model = CarModel;
-               this.limit = SpeedLimit;
-               
-       }
-    public void display(){
-      System.out.println("The car brand is: "+Brand);
-      System.out.println("The car model name is: "+Model);
-      System.out.println("The car speed limit is"+limit);
-   
-}
+    @Override
+    public void Engine(){
+        System.out.println(" Engine power per km is higher than a bike");
+    }
+    @Override
+    public void HP(){
+        System.out.println("Horse Power per km is about 1000kmph");
+    }
+    @Override
+    public int getTopSpeed(){
+        return 1000;
+    }
 }
