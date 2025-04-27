b73e87ad46476e312487b442db15986ddb5e018f
prajanlal <prajanswags@gmail.com>
2025-04-27 15:46:07 +0530

    Feat:Added some features to it

--- Interface/Car.java
+++ Interface/Car.java
@@ -1,5 +1,29 @@
 public class Car implements Vehicle {
     @Override
+    public void start(){
+        System.out.println("The car has been started");
+    }
+    @Override
+    public void stop(){
+        System.out.println("The car has been stopped");
+    }
+    @Override
+    public void accelerate(int increase){
+        System.out.println("The accleration has been started");
+    }
+    @Override
+    public void brake(int decrease){
+        System.out.println("The car has been stopped because of the brake ");
+    }
+    @Override
+    public void refuel(int fuelAdded){
+        System.out.println("The car has been refuelled");
+    }
+    @Override
+    public void displaystatus(){
+        System.out.println("The status");
+    }
+    @Override
     public void Engine(){
         System.out.println(" Engine power per km is higher than a bike");
     }
@@ -7,7 +31,7 @@ public class Car implements Vehicle {
     public void HP(){
         System.out.println("Horse Power per km is about 1000kmph");
     }
-    @Override
+   @Override
     public int getTopSpeed(){
         return 1000;
     }
