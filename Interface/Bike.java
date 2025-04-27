34dba762484c5c71b8b73978ff3d2cb009030a10
prajanlal <prajanswags@gmail.com>
2025-04-27 15:45:16 +0530

    Feat: Added more features to it

--- Interface/Bike.java
+++ Interface/Bike.java
@@ -1,14 +1,47 @@
-public class Bike implements Vehicle{ 
+ public class Bike implements Vehicle { 
+
+ @Override
+ public void start(){
+  System.out.println("The biek has been started");
+ }
+
+ @Override
+ public void stop(){
+  System.out.println("The bike has been stopped");
+ }
+
+ @Override
+ public void accelerate(int increase){
+  System.out.println("The acceleration has been starteed");
+ }
+
+ @Override
+ public void brake(int decrease){
+  System.out.println("It's time to brake");
+ }
+
+ @Override
+ public void refuel(int fuelAdded){
+  System.out.println("The bike fuel has been refuelled");
+ }
+
+ @Override
+ public void displaystatus(){
+  System.out.println("The status ");
+ }
+ 
     @Override
-    public void Engine(){
+    public void Engine() {
         System.out.println("Engine type is V7");
     }
+
     @Override
-      public void HP(){
+      public void HP() {
         System.out.println("Horse Power per km is 100 ");
       }
+
       @Override
-      public int getTopSpeed(){
+      public int getTopSpeed() {
         return 800;
       }
 }
