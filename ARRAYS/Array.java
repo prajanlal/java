4a9a3af00058f9dd8bc386accaebb262ed9c4675
prajanlal <prajanswags@gmail.com>
2025-04-27 11:03:22 +0530

    feat: Added a array programs

---
+++ ARRAYS/Array.java
@@ -0,0 +1,13 @@
+ 
+public  class Array {
+     
+    public static void main(String[] args) {
+        
+       int[] numbers = {10, 20, 30, 40, 50};
+             System.out.println("Array length: "+numbers.length);
+        for(int number: numbers){
+             System.out.println(number);
+        }
+    }
+}
+      
