2066fc01e552bdac13b11b52cf194a1bc5a97951
prajanlal <prajanswags@gmail.com>
2025-04-29 17:27:15 +0530

    Fix: Added an array file

---
+++ ARRAYS/Sortarray.java
@@ -0,0 +1,10 @@
+
+import java.util.Arrays;
+
+public class Sortarray  {
+       public static void main(String[] args){
+        int[] numbers = {11, 21, 31, 41, 51};
+           Arrays.sort(numbers);
+           System.out.println("Sorted array: "+Arrays.toString(numbers));
+                  }
+}
