5a9b3dd78b77fd4f00c29bb723b0b7a475c0316f
prajanlal <prajanswags@gmail.com>
2025-05-02 19:42:17 +0530

    Fix: changed some problems

--- ARRAYS/Car.java
+++ ARRAYS/Car.java
@@ -1,44 +1,13 @@
-public class Car   {
 
-     String Brand;
-     String Model;
-    int limit;
 
-       public void setBrand(String Brand){
-         this.Brand = Brand;
-        }
 
-        public String getBrand(){
-         return Brand;
-        }
+class Car {
 
-        public void setModel(String Model){
-         this.Model = Model;
-        }
+    Car(String bmw, String m4, int par) {
+    }
 
-        public String getModel(){
-         return Model;
-        }
+    void display() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
-        public void setlimit(int limit){
-         this.limit = limit;
-        }
-
-        public int getlimit(){
-         return limit;
-       }
-
-      public Car(String  Carbrand,String CarModel,int SpeedLimit) {
-               this.Brand = Carbrand;
-               this.Model = CarModel;
-               this.limit = SpeedLimit;
-               
-       }
-       
-    public void display(){
-    
-      System.out.println("Car Brand: "+Brand);
-      System.out.println("Car model : "+Model);
-      System.out.println("Car speed limit is: "+limit);
-      }
 }
