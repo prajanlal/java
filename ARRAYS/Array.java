2e3580acc539035856d874997f687057c2cb6e2c
prajanlal <prajanswags@gmail.com>
2025-05-02 19:36:01 +0530

    Feat: added extra values

--- ARRAYS/Array.java
+++ ARRAYS/Array.java
@@ -1,13 +1,14 @@
  
 public  class Array {
-     
     public static void main(String[] args) {
+
+       int[] numbers = {10, 20, 30, 40, 50, 60};
+       int sum = 0;
         
-       int[] numbers = {10, 20, 30, 40, 50};
-             System.out.println("Array length: "+numbers.length);
-        for(int number: numbers){
-             System.out.println(number);
-        }
+       for(int number : numbers) {
+        sum += number;
+
+       }
+        System.out.println("Sum of array elements: "+sum);
     }
 }
-      
