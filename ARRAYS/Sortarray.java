b734fdcc4d84e33858be59072642051a47c712cb
prajanlal <prajanswags@gmail.com>
2025-05-03 21:11:13 +0530

    Feat: Added some math solutions

--- ARRAYS/Sortarray.java
+++ ARRAYS/Sortarray.java
@@ -1,10 +1,11 @@
-
-import java.util.Arrays;
-
 public class Sortarray  {
        public static void main(String[] args){
-        int[] numbers = {11, 21, 31, 41, 51};
-           Arrays.sort(numbers);
-           System.out.println("Sorted array: "+Arrays.toString(numbers));
+        int[] numbers = {11, 21, 31, 41, 51, 61, 71};
+        int[] nbrs = {12, 29, 39, 4};
+        int[] result = new int[numbers.length + nbrs.length ];
+
+           System.arraycopy(numbers, 0, result, 0, numbers.length);
+           System.arraycopy(nbrs, 0, result ,numbers.length , nbrs.length);
+           
                   }
 }
