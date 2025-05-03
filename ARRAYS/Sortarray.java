9e038b6ae28fa8237ec38fdf5c426ccbb0f4968e
prajanlal <prajanswags@gmail.com>
2025-05-03 22:39:25 +0530

    Feat: Added 3 arrays

--- ARRAYS/Sortarray.java
+++ ARRAYS/Sortarray.java
@@ -1,23 +1,22 @@
-
 import java.util.Arrays;
 
 public class Sortarray  {
 
        public static void main(String[] args) { 
+              
        int[] array1 = {12, 2, 34, 21, 10};
        int[] array2 = {30, 21, 12};
-       int[] array3 = {20, 30, 40};
+       int[] array3 = {20, 21, 40, 52};
 
-       int[] result = new int[array1.length + array2.length +array3.length]; 
+   int totalLength = array1.length + array2.length + array3.length;
+
+       int[] result = new int[totalLength]; 
 
        System.arraycopy(array1, 0, result, 0, array1.length);
-       System.arraycopy(array2, 0, result, 0, array2.length);
-       System.arraycopy(array3, 0, result, 0, array3.length);
+       System.arraycopy(array2, 0, result, array1.length, array2.length);
 
-       System.arraycopy(array2, 0, result, 0, array2.length);
-       System.arraycopy(array2, 0, result, array1.length , array2.length, array3.length);
-
+         System.arraycopy(array3, 0, result, array1.length + array2.length, array3.length);
+     
        System.out.println("Combined Array: "+ Arrays.toString(result));
-
        }
        }
