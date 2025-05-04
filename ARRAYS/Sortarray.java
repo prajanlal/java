9db566dc8cc59f563b606eeca57fb89ad0112249
prajanlal <prajanswags@gmail.com>
2025-05-04 21:29:31 +0530

    Feat: Added array4

--- ARRAYS/Sortarray.java
+++ ARRAYS/Sortarray.java
@@ -1,22 +1,24 @@
+
 import java.util.Arrays;
 
-public class Sortarray  {
+public class Sortarray {
 
-       public static void main(String[] args) { 
-              
-       int[] array1 = {12, 2, 34, 21, 10};
-       int[] array2 = {30, 21, 12};
-       int[] array3 = {20, 21, 40, 52};
+    public static void main(String[] args) {
 
-   int totalLength = array1.length + array2.length + array3.length;
+        int[] array1 = {12, 42, 45, 23};
+        int[] array2 = {35, 32, 42, 25};
+        int[] array3 = {13, 22, 41, 25};
+        int[] array4 = {41, 40, 31, 100};
 
-       int[] result = new int[totalLength]; 
+        int totalLength = array1.length + array2.length + array3.length + array4.length;
 
-       System.arraycopy(array1, 0, result, 0, array1.length);
-       System.arraycopy(array2, 0, result, array1.length, array2.length);
+        int[] result = new int[totalLength];
 
-         System.arraycopy(array3, 0, result, array1.length + array2.length, array3.length);
-     
-       System.out.println("Combined Array: "+ Arrays.toString(result));
-       }
-       }
+        System.arraycopy(array1, 0, result, 0, array1.length);
+        System.arraycopy(array2, 0, result, array1.length, array2.length);
+
+        System.arraycopy(array3, 0, result, array1.length + array2.length, array3.length);
+        System.arraycopy(array4, 0, result, array1.length + array2.length + array3.length, array4.length);
+        System.out.println("Arrays: " + Arrays.toString(result));
+    }
+}
