a99b85db360a62a890dc284ddba2fa781f05b3ae
prajanlal <prajanswags@gmail.com>
2025-05-04 22:37:53 +0530

    Feat: added a new vehicle#

--- Interface/User.java
+++ Interface/User.java
@@ -1,84 +1,86 @@
 
 import java.util.Scanner;
 
-public class Interface {
+public class User {
 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
 
-          try (Scanner scanner = new Scanner(System.in)) {
-             Vehicle Bike = new Bike();
-             Vehicle Car = new Car();
+        try (Scanner scanner = new Scanner(System.in)) {
+            Vehicle Bike = new Bike();
+            Vehicle Car = new Car();
 
-             System.out.println("\n");
-             System.out.println("The Bike Behaviours");
-             System.out.println("\n");
-             
-             Bike.refuel( );
-             Bike.start();
-             Bike.HP();
-             Bike.Engine();
-             Bike.accelerate();
-             Bike.displaystatus();
-             Bike.brake();
-             Bike.stop();
+            System.out.println("\n");
+            System.out.println("The Bike Behaviours");
+            System.out.println("\n");
 
-             System.out.println("Bike Top Speed: "+Bike.getTopSpeed()+"km/h");
-             
-             System.out.println("\n");
-             System.out.println("The Car Behaviours");
-             System.out.println("\n");
-             
-             Car.Engine();
-             Car.HP();
-             
-             System.out.println("Car Top Speed: "+Car.getTopSpeed()+ "km/h");
-             
-             OUTER:
-             while (true) {
-                 System.out.println("\nVehicle Manager");
-                 System.out.println("1. Start a Car");
-                 System.out.println("2. Start a Bike");
-                 System.out.println("3. Exit ");
-                 System.out.println("Choose a vehicle type (1-4): ");
-                 String choice = scanner.nextLine().trim();
+            Bike.refuel();
+            Bike.start();
+            Bike.HP();
+            Bike.Engine();
+            Bike.accelerate();
+            Bike.displaystatus();
+            Bike.brake();
+            Bike.stop();
 
-                 Vehicle vehicle;
+            System.out.println("Bike Top Speed: " + Bike.getTopSpeed() + "km/h");
 
-                 switch (choice) {
+            System.out.println("\n");
+            System.out.println("The Car Behaviours");
+            System.out.println("\n");
 
-                     case "1" -> {
-                         vehicle = new Car();
-                         vehicle.accelerate();
-                         vehicle.Engine();
-                         vehicle.displaystatus();
-                         vehicle.HP();
-                         vehicle.getTopSpeed();
-                         vehicle.refuel();
-                         vehicle.start();
-                         vehicle.stop();
-                     }
+            Car.Engine();
+            Car.HP();
 
-                     case "2" -> {
-                         vehicle = new Bike();
-                         vehicle.accelerate();
-                         vehicle.displaystatus();
-                         vehicle.Engine();
-                         vehicle.HP();
-                         vehicle.start();
-                         vehicle.getTopSpeed();
-                         vehicle.brake();
-                         vehicle.stop();
-                     }
+            System.out.println("Car Top Speed: " + Car.getTopSpeed() + "km/h");
 
-                     case "3" -> {
+            OUTER:
+            while (true) {
+                System.out.println("\nVehicle Manager");
+                System.out.println("1. Start a Car");
+                System.out.println("2. Start a Bike");
+                System.out.println("3. Exit ");
+                System.out.println("Choose a vehicle type (1-4): ");
+                String choice = scanner.nextLine().trim();
 
-                         System.out.println("Exit ");
-                         break OUTER;
-                     }
+                Vehicle vehicle;
 
-                     default -> System.out.println("Invalid option, please try again");
-                 }
-             }
-         }
+                switch (choice) {
+
+                    case "1" -> {
+                        vehicle = new Car();
+                        vehicle.accelerate();
+                        vehicle.Engine();
+                        vehicle.displaystatus();
+                        vehicle.HP();
+                        vehicle.getTopSpeed();
+                        vehicle.refuel();
+                        vehicle.start();
+                        vehicle.stop();
+                    }
+
+                    case "2" -> {
+                        vehicle = new Bike();
+                        vehicle.accelerate();
+                        vehicle.displaystatus();
+                        vehicle.Engine();
+                        vehicle.HP();
+                        vehicle.start();
+                        vehicle.getTopSpeed();
+                        vehicle.brake();
+                        vehicle.stop();
+                    }
+                    case "3" -> {
+                        vehicle = new Train();
+                    }
+                    case "4" -> {
+                        System.out.println("Exit ");
+                        break OUTER;
+                    }
+
+                    default ->
+                        System.out.println("Invalid option, please try again");
+                }
+            }
+        }
     }
-} 
+}
