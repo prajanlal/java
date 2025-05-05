77af8b041fff12cb3185796ced3ca39c9741397a
prajanlal <prajanswags@gmail.com>
2025-05-05 22:39:08 +0530

    Added Frontend file for taskmanager

---
+++ Gui.java
@@ -0,0 +1,10 @@
+
+import javax.swing.JPanel;
+
+public class Gui {
+
+    JLabel label = new JLabel();
+    JText text = new JText();
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+}
