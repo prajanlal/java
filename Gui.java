b069ba8890961fa43f98d682609f1efa53ed49b0
prajanlal <prajanswags@gmail.com>
2025-05-11 21:49:13 +0530

    Feat: added some things

--- Gui.java
+++ Gui.java
@@ -9,31 +9,34 @@ import javax.swing.JTextField;
 public class Gui {
 
     public static void main(String[] args) {
+
         JFrame frame = new JFrame();
 
         frame.setSize(450, 450);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
 
         JPanel panel = new JPanel();
+
         frame.add(panel);
 
         placeComponents(panel);
+
+        frame.setVisible(true);
     }
 
     public static void placeComponents(JPanel panel) {
 
         panel.setLayout(null);
 
-        JLabel userLabel = new JLabel();
+        JLabel userLabel = new JLabel("User");
         userLabel.setBounds(10, 20, 80, 25);
         panel.add(userLabel);
 
-        JTextField userText = new JTextField();
+        JTextField userText = new JTextField(20);
         userText.setBounds(100, 20, 165, 25);
         panel.add(userText);
 
-        JLabel passwordLabel = new JLabel();
+        JLabel passwordLabel = new JLabel("Password");
         passwordLabel.setBounds(10, 50, 80, 25);
         panel.add(passwordLabel);
 
