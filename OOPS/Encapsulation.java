295ec119848428e62c61445751f092de21cfcef0
prajanlal <162490198+prajanlal@users.noreply.github.com>
2025-05-30 23:49:50 +0530

    Update Encapsulation.java

--- OOPS/Encapsulation.java
+++ OOPS/Encapsulation.java
@@ -1,20 +1,22 @@
 public class area {
+String shape;
     int length;
     int breadth;
     int area;
-    area(int length, int breadth) {
+    area(int length, int breadth, String shape) {
         this.length = length;
         this.breadth = breadth;
+        this.shape = shape;
     }
     public void getarea() {
-        int area = length * breadth;
+        int area = length * breadth * shape;
         System.out.println("area: " + area);
     }
 }
 
 class Main {
     public static void main(String[] args) {
-        area rectangle = new area(5, 6);
+        area rectangle = new area(5, 6, square);
         rectangle.getarea();
     }
 }
