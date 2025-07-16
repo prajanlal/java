52769ad2eea89cb16f95455b92a243fe9d18df94
prajanlal <162490198+prajanlal@users.noreply.github.com>
2025-07-16 17:31:15 +0100

    Update Bike.java

--- ARRAYS/Bike.java
+++ ARRAYS/Bike.java
@@ -4,7 +4,7 @@ public class Bike {
     public Bike(String kawasaki, String zX10R, int i, String japan) {
     }
 
-    void display() {
+  public  void display() { 
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
