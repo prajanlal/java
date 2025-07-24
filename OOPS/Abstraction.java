6441d5cf7cb76c76f9872fcaeec9b0d620fd8346
prajanlal <162490198+prajanlal@users.noreply.github.com>
2025-07-24 18:18:36 +0100

    Update Abstraction.java

--- OOPS/Abstraction.java
+++ OOPS/Abstraction.java
@@ -10,6 +10,11 @@ abstract class Animal {
             System.out.println("The cow says: moo moo");
         }
     }
+class Goat extends Animal {
+    public void goat{
+    System.out.println("The goat says: suii");
+    }
+}
       // main class
     class  Main{
         public static void main(String[] args) {
