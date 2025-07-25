3d5b2370bb5d2cace1a550d6b15b9dfa51ba37c0
prajanlal <162490198+prajanlal@users.noreply.github.com>
2025-07-25 18:26:54 +0100

    Update Abstraction.java

--- OOPS/Abstraction.java
+++ OOPS/Abstraction.java
@@ -19,5 +19,32 @@ class Car extends Vehicle {
     void startEngine() {
         System.out.println("Car engine started");
     }
+    void stopEngine() {
+    System.out.println("Car engine stopped");
+    }
+  void display() {
+      super.displayinfo();
+      System.out.println("Type: Car");
+      System.out.println("Doors:"+doors);
+  }
+}
+class Bike extends Vehicle() {
+boolean hasGear;
+
+    Bike(String brand,boolean hasGear) {
+        super(brand);
+        this.hasGear=hasGear;
+    }
+    void startEngine() {
+        System.out.println("Bike engine started with self-start");
+    }
+    void stopEngine(){
+        System.out.println("Bike engine stopped");
+    }
+    void kickStart() {
+        System.out.println("Kick-starting the bike");
+    }
+    
+      
     
     
