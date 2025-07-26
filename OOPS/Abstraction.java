e8f2a3098507dc2d3a1aa46d8133bc051ea5adc2
prajanlal <162490198+prajanlal@users.noreply.github.com>
2025-07-26 04:03:43 +0100

    Update Abstraction.java

--- OOPS/Abstraction.java
+++ OOPS/Abstraction.java
@@ -4,6 +4,7 @@ abstract class Vehicle {
      Vehicle (String brand ) {
        this.brand = brand;
      }
+    
     abstract void startEngine();
     abstract void stopEngine();
 
@@ -11,24 +12,29 @@ abstract class Vehicle {
        System.out.println("Brand: " +brand);
    }
 }
+
 class Car extends Vehicle {
     int doors;
     Car (String brand, int doors) {
         super(brand);
          this.doors = doors;
      }
+    
     void startEngine() {
         System.out.println("Car engine started");
     }
+    
     void stopEngine() {
     System.out.println("Car engine stopped");
     }
+    
   void display() {
      super.displayinfo();
       System.out.println("Type: Car");
       System.out.println("Doors:"+doors);
   }
 }
+
 class Bike extends Vehicle() {
     boolean hasGear;
 
@@ -36,21 +42,25 @@ class Bike extends Vehicle() {
         super(brand);
          this.hasGear=hasGear;
     }
+    
     void startEngine() {
         System.out.println("Bike engine started with self-start");
     }
     void stopEngine(){
         System.out.println("Bike engine stopped");
     }
+    
     void kickStart() {
         System.out.println("Kick-starting the bike");
     }
+    
     void display() {
        super.displayinfo();
         System.out.println("Type: Bike");
         System.out.println("Has Gear: "+(hasGear ? "Yes" : "No"));
     }
 }
+
 public class Main {
     public static void main(String[] args) {
         Car car = new Car("Toyota",4);
