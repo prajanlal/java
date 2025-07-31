5c9cf3d6b04365b4807fa952ec6eae5c6a37abd7
prajanlal <prajanswags@gmail.com>
2025-07-31 01:27:23 +0100

    Feat: Added different forms

--- OOPS/Polymorphism.java
+++ OOPS/Polymorphism.java
@@ -1,16 +1,16 @@
 public class Polymorphism{
     public void sportcar() {
-        System.out.println("bmw is a sports car");
+        System.out.println("bmw is a sports car that is very cool nd fast like a king fpr all them");
     }
 
     class topengine {
         public void sportcar() {
-            System.out.println("bmw is a super sport car");
+            System.out.println("bmw is a super sport car thas has a top engine on the planet");
         }
 
         class topspeed {
             public void speed() {
-                System.out.println("BUGGATI has world record for top speed");
+                System.out.println("BUGGATI has world record for top speed on the planet better than bmw");
             }
         }
     }
