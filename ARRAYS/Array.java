24a2e3e04e9017f297b0d09f63e461d6bc0caebb
prajanlal <162490198+prajanlal@users.noreply.github.com>
2025-08-27 22:37:07 +0530

    Update Array.java

--- ARRAYS/Array.java
+++ ARRAYS/Array.java
@@ -1,7 +1,7 @@
  
 public  class Array {
     public static void main(String[] args) {
-       int[] numbers = {5, 10, 20, 30, 40, 50, 60};
+       int[] numbers = {5, 10,15, 20, 30, 40, 50, 60};
        int sum = 0;
         
        for(int number : numbers) {
